7442e5ed41189e04942b0f7dc21417a339453f9b
Sana Parveen <misana348@gmail.com>
2024-05-06 21:20:31 +0530

    Day 5-6

--- 4/checkTriangleType.js
+++ 4/checkTriangleType.js
@@ -1,7 +1,4 @@
-Write a function called checkTriangleType that takes three parameters representing the lengths of the sides of a triangle. The function should return a string indicating the type of triangle: "equilateral", "isosceles", or "scalene".
 
-
-//* Solution:
 function checkTriangle(a, b, c) {
   if (a === b && b === c) return "equilateral";
   else if (a === b || b === c || a === c) return "isoceles";
