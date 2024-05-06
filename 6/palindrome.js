a579c774d232553e72a0335a5a6c51a6fb9d0d8f
Sana Parveen <misana348@gmail.com>
2024-05-06 21:23:51 +0530

    Day 6

---
+++ 6/palindrome.js
@@ -0,0 +1,20 @@
+function palindrome(str){
+    str = str.toLowerCase();
+    let i = 0;
+    let j = str.length - 1;
+    while(i<j){
+        if(str[i]!=str[j]){
+            console.log("No, it is not Palindrome");
+            return;
+        }
+
+        i++;
+        j--;
+
+    }
+    console.log("Yes, it is a Palindrome");
+
+
+}
+let str = "racecar";
+palindrome(str);
