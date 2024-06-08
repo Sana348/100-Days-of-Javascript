bcefdf5be64415377e981bf6b07c0cb8c7290a58
Sana Parveen <70599479+Sana348@users.noreply.github.com>
2024-06-08 22:50:18 +0530

    Day 36

---
+++ 36/index.js
@@ -0,0 +1,14 @@
+arr = ['ydhdksms', 'efth', '123', 'abce'];
+    
+// Function to create object
+function toObject(arr) {
+    // target object
+    let rv = {};
+    // Traverse array using loop
+    for (let i = 0; i < arr.length; ++i)
+        // Assign each element to object
+        rv[i] = arr[i];
+    return rv;
+}
+// Display output
+console.log(JSON.stringify(toObject(arr)));
