7aa5fac6c2dd0a2f9093002940c169c48f016a69
Sana Parveen <misana348@gmail.com>
2024-06-28 17:35:40 +0530

    Day 46

---
+++ 45/numberValidation.js
@@ -0,0 +1,9 @@
+const validateIndianMobileNumber = (number) => {
+  return /^[6-9][\d]{9}$/.test(number);
+};
+
+// Test Cases
+console.log(validateIndianMobileNumber("9876543210")); // Expected output: true
+console.log(validateIndianMobileNumber("8923456789")); // Expected output: true
+console.log(validateIndianMobileNumber("897654321")); // Expected output: false
+console.log(validateIndianMobileNumber("78965432107")); // Expected output: false
