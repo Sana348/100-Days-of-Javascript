dbf4ec601421fac1ea23f82254fc345ded2c3c2a
Sana Parveen <70599479+Sana348@users.noreply.github.com>
2024-07-25 22:02:33 +0530

    Day 45

--- 45/numberValidation.js
+++ 45/numberValidation.js
@@ -6,4 +6,4 @@ const validateIndianMobileNumber = (number) => {
 console.log(validateIndianMobileNumber("9876543210")); // Expected output: true
 console.log(validateIndianMobileNumber("8923456789")); // Expected output: true
 console.log(validateIndianMobileNumber("897654321")); // Expected output: false
-console.log(validateIndianMobileNumber("78965432107")); // Expected output: false
+console.log(validateIndianMobileNumber("732107")); // Expected output: false
