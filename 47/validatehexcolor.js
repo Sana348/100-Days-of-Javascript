c1d1bfba1576b78453c057056948bd9807f65924
Sana_Parveen <misana348@gmail.com>
2024-07-31 17:07:31 +0530

    Day 48

---
+++ 47/validatehexcolor.js
@@ -0,0 +1,13 @@
+function isValidHexColor(hex) {
+    const hexPattern = /^#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$/;
+    return hexPattern.test(hex);
+}
+
+// Test cases
+console.log(isValidHexColor("#FFFFFF")); // true
+console.log(isValidHexColor("#FFF"));    // true
+console.log(isValidHexColor("#ff5733")); // true
+console.log(isValidHexColor("#123"));    // true
+console.log(isValidHexColor("#12345G")); // false
+console.log(isValidHexColor("123456"));  // false
+console.log(isValidHexColor("#1234"));   // false
